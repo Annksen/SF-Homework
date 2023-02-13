e0961925b709b833f7987afc0d243af506ee75f8
AnnKsen <nuwa50109@gmail.com>
2023-02-13 21:36:22 +0300

    Initial Commit

---
+++ task2.py
@@ -0,0 +1,56 @@
+# first_name = input("Введите ваше имя:")
+# last_name = input("Введите вашу фамилию:")
+# age = input("Введите ваш возраст:")
+# city = input("Введите город проживания:")
+#
+# print('')
+# print('Привет,', first_name, last_name, '!')
+# print('')
+# print("Ваш профиль:")
+# print("Возраст:", age, "лет")
+# print('Город:', city)
+
+# print(round(11*2.5/3, 2))
+
+# pi = 3.14159
+# print(round(pi**2/2))
+
+# numbers = input('Введите числа через пробел:')
+# numbers_split = numbers.split()
+# numbers_lines = '\n'.join(numbers_split)
+# print(numbers_lines)
+
+# pi = 31.4159265
+# print('%.4e' % (pi))
+#
+#
+# title = input("Введите название книги:")
+# author = input("Введите автора книги:")
+# year = input("Введите год выпуска:")
+# book = {'title' : title,
+#         'author' : author,
+#         'year' : year}
+# print(book)
+
+# text = input("Введите текст")
+# b = list(set(text))
+# print("Количество уникальных символов", len(b))
+#
+# a = input("Введите числа через пробел").split()
+# b = input("Введите числа через пробел").split()
+# a_int_list = list(map(int, a))
+# b_int_list = list(map(int, b))
+# a_set = set(a_int_list)
+# b_set = set(b_int_list)
+# list1 = a_set.symmetric_difference(b_set)
+# print(list(list1))
+#
+# L = ['a', 'b', 'c']
+# print(id(L))
+#
+# L.append('d')
+# print(id(L))
+
+# a = 5
+# b = 3+2
+# print(id(a) - id(b))
